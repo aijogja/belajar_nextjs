0643c34c9a652070de5cbd691ae254d8e4dde577
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-11 20:42:34 +0700

    try to use array data in state

--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,7 @@ class Login extends React.Component {
         return(
             <div>
                 <Layout>
-                    <form className="col s12">
+                    <form className="col offset-s3 s6">
                         <div className="row">
                             <div className="input-field col s12">
                             <input id="email" type="email" className="validate" />
--- pages/produk.js
+++ pages/produk.js
@@ -3,15 +3,31 @@ import Layout from '../components/layout'
 import {Sidenav} from '../components/nav'
 
 class Produk extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            products : [
+                { id : 1, name: 'Xiaomi'},
+                { id : 2, name: 'Gula'},
+                { id : 3, name: 'Sepeda'},
+            ]
+        }
+    }
 
     render() {
+        const {products} = this.state
+        // console.log(products)
         return(
-            <div>
-                <Layout>
-                    <Sidenav />
-                    <div className="col s9">DIsini diisi list produk</div>
-                </Layout>
-            </div>
+            <Layout>
+                <Sidenav />
+                <div className="col s9">
+                    {
+                        products.length && products.map((product) => (
+                            <div key={product.id}>{product.name}</div>
+                        ))
+                    }
+                </div>
+            </Layout>
         )
     }
 }
