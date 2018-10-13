c7c063585c0c1f14e9ee2219bc1814e103923cb7
Ai Jogja <cah7x_jogja@yahoo.com>
2018-10-13 15:59:39 +0700

    get data from api using axios

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="static/css/materialize.min.css" />
           <link rel="stylesheet" href="static/css/style.css" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
           <style>{`body { margin: 0 } /* custom! */`}</style>
         </Head>
         <body className="custom_class">
--- pages/produk.js
+++ pages/produk.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { Sidenav } from '../components/nav'
+import axios from 'axios'
 
 class Produk extends React.Component {
 	constructor(props) {
 		super(props)
+		// set the default state for products
 		this.state = {
-			products: [
-				{ id: 1, name: 'Xiaomi' },
-				{ id: 2, name: 'Gula' },
-				{ id: 3, name: 'Sepeda' },
-			]
+			products: [],
 		}
+	}
+
+	componentDidMount = async () => {
+		// axios is used for call the api (client request)
+		const products = await axios.get('http://localhost:8000/api/produk/')
+		// this setState is used to set (replace) the default props products
+		this.setState({products: products.data})
 	}
 
 	render() {
 		const { products } = this.state
-		// console.log(products)
 		return (
 			<Layout>
 				<Sidenav />
 				<div className="col s9">
 					{
-						products.length && products.map((product) => (
-							<div key={product.id}>{product.name}</div>
+						products.map((product) => (
+							<div key={product.id}>{product.nama}</div>
 						))
 					}
 				</div>
