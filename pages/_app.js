3aba7e9a828505a2b68ad55229c01b4fe2850f85
Ai Jogja <cah7x_jogja@yahoo.com>
2018-11-06 15:20:29 +0700

    update cart

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import axios from 'axios'
+const token = '8e4c7d388ce7d0666c7b72668762bd08dced626c'
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -16,17 +18,59 @@ export default class MyApp extends App {
 		super(props)
 		// set the default state for products
 		this.state = {
-			totalcart: 20,
+      totalCart: 0,
+      idOrder:'',
     }
     // set prop totalcart
     this.totalcart = 10
   }
+
   
-  // Ini adalah fungsi (struktur dari ES6)
-  // props dalam bentuk fungsi ini tidak bisa langsung di render
-  // ini fungsi nanti yang akan dipanggil di `addcart`
   ganticart = (isi) => {
-    this.setState({totalcart:isi})
+    /*
+    Ini adalah fungsi (struktur dari ES6)
+    props dalam bentuk fungsi ini tidak bisa langsung di render
+    ini fungsi nanti yang akan dipanggil di `addcart`
+    */
+    this.setState({totalCart:isi})
+  }
+
+  hitungTotalCart = (cart) => {
+    /*
+    Fungsi untuk menghitung total qty yang ada di cart
+    */
+    let qty = 0
+    cart.map((item) => {
+      qty += item.qty
+    })
+    return qty
+  }
+
+  getCart = async () => {
+		/*
+		Fungsi ini digunakan untuk menampilkan cart
+		(cart yaitu orderan dengan status cart)
+		*/
+    const existOrder = await axios.get(
+      'http://localhost:8000/api/order/?status=cart',
+      { headers : {"Authorization": "Token " + token}}
+    )
+    if(existOrder.data.length > 0){
+      const order = existOrder.data[existOrder.data.length - 1]
+      const listBarang = await axios.get(
+        `http://localhost:8000/api/order/${order.id}/barang/`,
+        { headers : {"Authorization": "Token " + token}}
+      )
+      const totalCart = this.hitungTotalCart(listBarang.data)
+      this.setState({
+        idOrder: order.id,
+        totalCart
+      })
+    }
+  }
+
+  componentDidMount = async () => {
+    await this.getCart()
   }
 
   render () {
@@ -36,8 +80,10 @@ export default class MyApp extends App {
       <Container>
         <Component
         {...pageProps}
-        totalcart={this.state.totalcart}
+        totalcart={this.state.totalCart}
         ganticart={this.ganticart}
+        idOrder={this.state.idOrder}
+        getCart={this.getCart}
         />
       </Container>
     )
--- pages/keranjang.js
+++ pages/keranjang.js
@@ -2,40 +2,107 @@ import React from 'react'
 import Layout from '../components/layout'
 import { Sidebar } from '../components/nav'
 import axios from 'axios'
+const token = '8e4c7d388ce7d0666c7b72668762bd08dced626c'
+
 
 class Keranjang extends React.Component {
 
+  constructor(props) {
+		super(props)
+		// set the default state for products
+		this.state = {
+			listBarang: [],
+		}
+	}
+
+	componentDidMount = async () => {
+    await this.props.getCart()
+    await this.getCart()
+  }
+
+  tambahQty = async (cart) => {
+    const orderBarang = await axios.post(
+      `http://localhost:8000/api/order/${this.props.idOrder}/barang/`,
+      {
+        "produk": cart.produk.id,
+        "qty": 1
+      },
+      { headers : {"Authorization": "Token " + token}}
+    )
+    await this.props.getCart()
+  }
+
+  kurangQty = async (cart) => {
+    const orderBarang = await axios.post(
+      `http://localhost:8000/api/order/${this.props.idOrder}/barang/`,
+      {
+        "produk": cart.produk.id,
+        "qty": -1
+      },
+      { headers : {"Authorization": "Token " + token}}
+    )
+    await this.props.getCart()
+  }
+
+  hapusBarang = async (id) => {
+    await axios.delete(
+      `http://localhost:8000/api/order/${this.props.idOrder}/barang/${id}`,
+      { headers : {"Authorization": "Token " + token}}
+    )
+    await this.props.getCart()
+    await this.getCart()
+  }
+
+  getCart = async () => {
+    const listBarang = await axios.get(
+      `http://localhost:8000/api/order/${this.props.idOrder}/barang/`,
+      { headers : {"Authorization": "Token " + token}}
+    )
+    this.setState({listBarang: listBarang.data})
+  }
+
+  updateQty = (id) => {
+    console.log(id)
+  }
+
 	render() {
+    const { listBarang } = this.state
 		return (
 			<Layout {...this.props}>
-				<div className="col s12 l8">
-          <div className="card horizontal">
-          <div className="card-image">
-            <img src="static/img/no-image.jpg" />
-          </div>
-          <div className="card-stacked">
-            <div className="card-content">
-              <span className="card-title">Xiaomi Redmi 3 </span>
-              <div>2000.000</div>
-            </div>
-            <div className="card-action">
-              <div className="row">
-                <div className="col s2 l2 center-align">
-                <a className="btn-floating waves-effect waves-light blue lighten-3"><i className="material-icons">remove</i></a>
+        <div className="col s12 l8">
+          {
+            listBarang.map((cart) => {
+              return (
+                <div className="card horizontal" key={cart.id}>
+                  <div className="card-image">
+                    <img src="static/img/no-image.jpg" />
+                  </div>
+                  <div className="card-stacked">
+                    <div className="card-content">
+                      <span className="card-title">{cart.produk.nama}</span>
+                      <div>{parseInt(cart.harga)}</div>
+                    </div>
+                    <div className="card-action">
+                      <div className="row">
+                        <div className="col s2 l2 center-align">
+                        <a className="btn-floating waves-effect waves-light blue lighten-3" onClick={() => this.kurangQty(cart)}><i className="material-icons">remove</i></a>
+                        </div>
+                        <div className="col s2 l2 center-align">
+                          <input className="center-align" type="text" value={cart.qty} onChange={() => this.updateQty(cart.id)} />
+                        </div>
+                        <div className="col s2 l2 center-align">
+                          <a className="btn-floating waves-effect waves-light blue lighten-3" onClick={() => this.tambahQty(cart)}><i className="material-icons">add</i></a>
+                        </div>
+                        <div className="col s6 l6 right-align">
+                        <a className="btn-floating waves-effect waves-light red" onClick={() => this.hapusBarang(cart.id)}><i className="material-icons">delete</i></a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col s2 l2 center-align">
-                  <input className="center-align" type="text" />
-                </div>
-                <div className="col s2 l2 center-align">
-                  <a className="btn-floating waves-effect waves-light blue lighten-3"><i className="material-icons">add</i></a>
-                </div>
-                <div className="col s6 l6 right-align">
-                <a className="btn-floating waves-effect waves-light red"><i className="material-icons">delete</i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              )
+            })
+          }
         </div>
         <div className="col s12 l4">
           <div className="card">
